f966112a755bdad6c3c8e2cda79f9794bf3b26ab
🍋 <21185192+03251112@users.noreply.github.com>
2025-03-07 14:16:39 +0800

    cs

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/screenshot', async (req, res) => {
     await page.setViewport({
       width: 1920,
       height: 1080,
-      deviceScaleFactor: 4,
+      deviceScaleFactor: 1,
     });
     await page.goto('http://baidu.com'); // 替换为你想截图的URL
     
